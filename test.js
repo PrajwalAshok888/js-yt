eef392d86dafc78ef2bad0e18607b144ac987a82
travelblog666 <145565199+travelblog666@users.noreply.github.com>
2024-03-20 13:52:24 +0530

    added a simple manual form

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("Hitesh choudary");
+let firstName="Prajwal"
+let lastName="A"
+let email="pythor666@gmai.com"
+let password="Pass1234$"
+let county="India"
+let state="Karnataka"
+let isGoogleLoggedIn=true
+let age=33
+
+console.table([firstName,lastName,email,password,county,state,isGoogleLoggedIn,age])
