fbf9a57595355c5d19c44910e62e594db524bed8
travelblog666 <145565199+travelblog666@users.noreply.github.com>
2024-03-20 14:15:15 +0530

    % added

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-let firstName="Prajwal"
-let lastName="A"
-let email="pythor666@gmai.com"
-let password="Pass1234$"
-let county="India"
-let state="Karnataka"
-let isGoogleLoggedIn=true
-let age=33
+// let firstName="Prajwal"
+// let lastName="A"
+// let email="pythor666@gmai.com"
+// let password="Pass1234$"
+// let county="India"
+// let state="Karnataka"
+// let isGoogleLoggedIn=true
+// let age=33
 
-console.table([firstName,lastName,email,password,county,state,isGoogleLoggedIn,age])
+// console.table([firstName,lastName,email,password,county,state,isGoogleLoggedIn,age])
+
+let listingPrice=799
+let sellingprice=199
+let discountPercentage=((listingPrice-sellingprice)/listingPrice)*100
+console.log(`${Math.round(discountPercentage)}% OFF`);
