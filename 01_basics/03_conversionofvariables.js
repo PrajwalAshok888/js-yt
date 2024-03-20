ef730c8c53be54a91568e247afa3fa0bf8e2c923
Prajwal A <117015923+PrajwalAshok888@users.noreply.github.com>
2024-03-20 10:33:23 +0000

    changes made to add operations and type coercion

--- 01_basics/03_conversionofvariables.js
+++ 01_basics/03_conversionofvariables.js
@@ -1,11 +1,11 @@
-let score = "hitesh"
+let score = "5"
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+let valueInNumber = -score
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -26,3 +26,50 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"aa");
+
+let num1= num2= num3=2+2
+
+
+
+let gameCounter = 100
+
+// console.log(gameCounter++);
+y=gameCounter
+// console.log(y)
+// console.log(gameCounter)
+// console.table([num1,num2,num3])
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
