181a46e1f22adc41ac1319537f69dccd5e3696a0
Prajwal A <117015923+PrajwalAshok888@users.noreply.github.com>
2024-03-20 17:38:02 +0000

    adde stack and heap memory def

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,11 +35,30 @@ const myFunction = function(){
 // console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
-console.log(typeof myFunction)
-console.log(typeof score)
-console.log(typeof isLoggedIn)
-console.log(typeof outsideTemp)
-console.log(typeof userEmail)
-console.log(typeof id)
-console.log(typeof heros)
-console.log(typeof myObj)
+// console.log(typeof myFunction)
+// console.log(typeof score)
+// console.log(typeof isLoggedIn)
+// console.log(typeof outsideTemp)
+// console.log(typeof userEmail)
+// console.log(typeof id)
+// console.log(typeof heros)
+// console.log(typeof myObj)
+// =============================================
+// Stack->Primitive    Heap->Non Primitive
+let userName="hello"
+let adddedName=userName//here the copy of userName is assigned
+// console.log(userName)
+// console.log(adddedName)
+
+adddedName="Prajwal";
+// console.log(userName)
+// console.log(adddedName)//modification of the copy is displayed
+
+let sell = {
+    email:"pythor@gmail.com"
+}
+let buy=sell//here the reference(address) of the sell memory is assigned
+
+console.log(sell,buy)
+buy.email="prajwala.2020@vemanait.edu.in"
+console.log(sell,buy)
