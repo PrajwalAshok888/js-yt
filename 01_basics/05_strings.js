e22d2c75eb8d12b178a5fc39e91044b17d321191
Prajwal A <117015923+PrajwalAshok888@users.noreply.github.com>
2024-03-21 06:55:35 +0000

    added strings file

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+// let firstName="Prajwal"
+// let age =22
+
+// console.log(firstName+age)
+
+//string interpolations
+// console.log(`${firstName}${age}`)
+
+//new way of declaring strins
+// let firstName = new String("Prajwal")
+// console.log(firstName)
+// console.log(firstName.__proto__)
+// console.log(firstName[3])
+// console.log(typeof firstName)
+// console.log(firstName.toLowerCase())
+// console.log(firstName.toUpperCase())
+// console.log(firstName)
+
+//this will not change the original sring because this is a primitive datatype even though its an object
+// let fullName = firstName
+// console.log(firstName)
+// console.log(fullName.concat(" dev"))
+// let firstname="prajwal"
+// console.log(typeof(firstname.length))//number
