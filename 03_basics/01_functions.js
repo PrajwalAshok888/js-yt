52a1936a5b0c9bee8a47496a61457f37f1ae6705
Prajwal A <117015923+PrajwalAshok888@users.noreply.github.com>
2024-04-04 21:27:01 +0000

    added objects and array functions in function folder

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,15 +50,49 @@ const result = addTwoNumbers(3, 5)
 // console.log(loginUserMessage("hitesh"))
 
 
-function loginUserMessage(username){
-    if(!username){
-        console.log("PLease enter a username");
+// function loginUserMessage(username){
+//     if(!username){
+//         console.log("PLease enter a username");
         
-    }
-    else{
-        return `${username} just logged in`
-    }
+//     }
+//     else{
+//         return `${username} just logged in`
+//     }
     
-}
+// }
 
-console.log(loginUserMessage("Prajwal"))
+// console.log(loginUserMessage("Prajwal"))
+//function to access objects
+const price={
+    username:"Prajwal",
+    price:299
+}
+function calculateCartPrice(anyobject){
+    return `${anyobject.username} has a total price of ${anyobject.price}`
+}
+// console.log(calculateCartPrice(price))
+//function to access objects other way
+
+function calculateCartPrice1(anyobject){
+    return `${anyobject.username} has a total price of ${anyobject.price}`
+}
+// console.log(calculateCartPrice({
+//     username:"Tushar",
+//     price:999
+
+// }))
+
+
+function addCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(addCartPrice(2,133,224,200,12402))
+
+//function to access arrays
+const myNewarray=[131,214,241,141]
+function getArray(anyArray){
+    return `${anyArray[0]},${anyArray[3]}`
+}
+// console.log(getArray(myNewarray))
+//other way to acces array in functions
+console.log(getArray([200,11,111,3,4,2,]))
